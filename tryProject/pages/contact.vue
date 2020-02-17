2acb36ee54942d52b2433cfbbb4da4919256e22b
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-02-17 15:45:22 +0100

    add content in the page contact.vue

--- tryProject/pages/contact.vue
+++ tryProject/pages/contact.vue
@@ -1,11 +1,23 @@
 <template>
-    <div>
-        
-    </div>
+  <section class="container">
+    <h1>This is a page Contact</h1>
+    <p class="subheading">this is a test</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos sapiente eius temporibus, praesentium quae optio, autem deleniti quam dolorum qui hic</p>
+  </section>
 </template>
-
 <script>
+
+
 export default {
-    
+      head(){
+          return{
+              title:"About nuxt",
+              meta:[
+                  {name:'description', content:'this is my page contact'},
+                  {name:'keywords', content:'about nuxt, nuxt infos'},
+
+              ]
+          }
+      }
 }
 </script>
