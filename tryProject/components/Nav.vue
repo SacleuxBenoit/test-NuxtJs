2d4fab7a9291d027de6cddaec479333effda04bd
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-02-17 11:48:13 +0100

    add a nav bar in the componenent Nav.vue

--- tryProject/components/Nav.vue
+++ tryProject/components/Nav.vue
@@ -1,6 +1,17 @@
 <template>
     <div>
-    
+        <header>
+            <nuxt-link to="/">Nuxty</nuxt-link>
+            <nav>
+                <ul>
+                    <li><nuxt-link to="/">Home</nuxt-link></li>
+                    <li><nuxt-link to="about">About</nuxt-link></li>
+                    <li><nuxt-link to="services">Services</nuxt-link></li>
+                    <li><nuxt-link to="contact">Contact</nuxt-link></li>
+
+                </ul>
+            </nav>
+        </header>
     </div>
 </template>
 
