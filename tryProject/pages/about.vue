e66415012d2bfccc3fb4dfd44fc2110d5346f1d8
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-02-17 13:20:17 +0100

    add content in the template and script of about.vue

--- tryProject/pages/about.vue
+++ tryProject/pages/about.vue
@@ -1,15 +1,16 @@
 <template>
-    <div>
-        
-    </div>
+  <section class="container">
+    <h1>This is an about page</h1>
+    <p class="subheading">this is a test</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos sapiente eius temporibus, praesentium quae optio, autem deleniti quam dolorum qui hic</p>
+  </section>
 </template>
-
 <script>
+
+
 export default {
-    
+  components: {
+
+  }
 }
 </script>
-
-<style>
-
-</style>
