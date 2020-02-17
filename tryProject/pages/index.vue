21be224b8b4a020e579edf2b5dcd10fc86da15ab
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-02-17 13:07:03 +0100

    add another paragraph with a lorem ipsum in it in pages/index.vue

--- tryProject/pages/index.vue
+++ tryProject/pages/index.vue
@@ -2,6 +2,7 @@
   <section class="container">
     <h1>The best Nuxt</h1>
     <p class="subheading">We've got the best Nuxt's</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos sapiente eius temporibus, praesentium quae optio, autem deleniti quam dolorum qui hic, obcaecati ex earum reiciendis ea? Doloribus porro repudiandae iusto!</p>
   </section>
 </template>
 <script>
