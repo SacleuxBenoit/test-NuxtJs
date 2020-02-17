785712695da34adcab924ce9ced2026cb0fb7cee
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-02-17 16:37:57 +0100

    add the import of the component Footer in layouts/default.vue

--- tryProject/layouts/default.vue
+++ tryProject/layouts/default.vue
@@ -1,15 +1,19 @@
 <template>
   <div>
     <Nav/>
+
     <nuxt/>
+          <Footer/>
   </div>
 </template>
 
 <script>
 import Nav from "../components/Nav";
+import Footer from "../components/Footer"
 export default {
   components:{
-    Nav
+    Nav,
+    Footer
   }
   
 }
